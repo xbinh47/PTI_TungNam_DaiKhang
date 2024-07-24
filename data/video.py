632dc5fd095c778245dd7624fbd2e535db5aa3f3
xbinh47 <binhnguyenxuan47@gmail.com>
2024-07-25 01:31:48 +0700

    data

--- data/video.py
+++
@@ -1,9 +0,0 @@
-
-class Movie ():
-    def __init__(self, id, name, link, release_date, genre, img) -> None:
-        self.id = id
-        self.name = name
-        self.release_date = release_date
-        self.genre = genre
-        self.link = link
-        self.img = img
